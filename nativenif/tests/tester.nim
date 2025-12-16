2ddfd98d8ac99eac4a93754cd252cc1cc4393183
Clemente90 <molten_mg@yahoo.com>
2025-12-16 09:49:01 -0800

    Add call instruction tests and fix procedure epilogues

--- nativenif/tests/tester.nim
+++ nativenif/tests/tester.nim
@@ -57,6 +57,10 @@ else:
   execExpectOutput("./tests/string_pointer_field", "Hello\n")
   exec "nim c -r src/nifasm/nifasm tests/message_inline_array.nif"
   execExpectOutput("./tests/message_inline_array", "Ping\n")
+  exec "nim c -r src/nifasm/nifasm tests/call_hello_chain.nif"
+  execExpectOutput("./tests/call_hello_chain", "Hello through calls\n")
+  exec "nim c -r src/nifasm/nifasm tests/call_multi_result.nif"
+  exec "./tests/call_multi_result"
   exec "nim c -r src/nifasm/nifasm tests/call_result_binding.nif"
   execExpectFailure("nim c -r src/nifasm/nifasm tests/double_bind.nif", "Register RAX is already bound to variable 'x.0'")
   execExpectFailure("nim c -r src/nifasm/nifasm tests/triple_bind.nif", "Register RAX is already bound to variable 'x.0'")
@@ -65,3 +69,5 @@ else:
   execExpectFailure("nim c -r src/nifasm/nifasm tests/missing_result_binding.nif", "Missing result binding: ret.0")
   execExpectFailure("nim c -r src/nifasm/nifasm tests/stack_result_binding.nif", "Result 'ret.0' must be bound to a register")
   execExpectFailure("nim c -r src/nifasm/nifasm tests/result_type_mismatch.nif", "Type mismatch:")
+  execExpectFailure("nim c -r src/nifasm/nifasm tests/call_missing_argument.nif", "Missing argument: arg.1")
+  execExpectFailure("nim c -r src/nifasm/nifasm tests/call_duplicate_result_binding.nif", "Duplicate result binding: ret.0")
